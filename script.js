a1107250caf7713eb8ccdc9adff3087bd49ecfb0
LAM HANG YI <leannelam@147-12-253-113-on-nets.com>
2022-07-26 08:06:46 +0800

    case-insensitive

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 let answer = prompt("What's your choice?");
 
 function getComputerChoice () {
-    const myArray=["Rock" , "Paper", "Scissors"]
+    const myArray=["rock" , "paper", "scissors"]
     
     const random = Math.floor(Math.random() * myArray.length);
     return myArray[random]
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "Rock" && computerSelection == "Paper") {
-        return("You Lose! Paper beats Rock");
-    } else if (playerSelection == 'Paper' && computerSelection == "Scissors") {
-        return("You Lose! Scissors beats Paper");
-    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
-        return("You Lose! Rock beats Scissors");
-    }  else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-        return("You Win! Rock beats Scissors");
-    }  else if (playerSelection == "Paper" && computerSelection == "Rock") {     
-        return("You Win! Paper beats Rock");
-    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-        return("You Win! Scissors beats Paper");
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        return(computerSelection + " You Lose! Paper beats Rock");
+    } else if (playerSelection == 'paper' && computerSelection == "scissors") {
+        return(computerSelection + " You Lose! Scissors beats Paper");
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return(computerSelection + " You Lose! Rock beats Scissors");
+    }  else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return(computerSelection + " You Win! Rock beats Scissors");
+    }  else if (playerSelection == "paper" && computerSelection == "rock") {     
+        return(computerSelection + " You Win! Paper beats Rock");
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return(computerSelection + " You Win! Scissors beats Paper");
+    } else if (playerSelection === computerSelection) {
+        return(computerSelection + " It's tie");
     } else {
-        return("It's tie")
+        return ("Enter again");
     }
 }
 
-const playerSelection = answer;
+const playerSelection = answer.toLowerCase();
 const computerSelection = getComputerChoice();
 
 
