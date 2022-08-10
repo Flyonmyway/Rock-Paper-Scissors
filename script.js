e0be29e14fca91874da072657b175e125a63d959
LAM HANG YI <leannelam@LAMs-MacBook-Air.local>
2022-08-10 15:42:17 +0800

    finished layout

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let userScore = 0;
 let computerScore = 0;
+let roundCounter = 0;
 const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector('.score-board');
@@ -7,7 +8,9 @@ const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById('r');
 const paper_div = document.getElementById('p');
 const scissors_div = document.getElementById('s');
-
+const roundCount_div = document.querySelector(".roundCount > p");
+const roundAnn_p = document.querySelector(".resultAnnounce > p");
+const choices_div = document.querySelector(".choices");
 
 function getComputerChoice() {
     const choices = ['r', 'p', 's'];
@@ -29,7 +32,7 @@ function win(userChoice, computerChoice) {
     result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}. You win!`
     userChoice_div.classList.add('green-glow');
     setTimeout(function() { userChoice_div.classList.remove('green-glow')}, 300); 
-    }
+}
 
 function loss(userChoice, computerChoice) {
     const userChoice_div = document.getElementById(userChoice);
@@ -69,7 +72,37 @@ switch (userChoice + computerChoice) {
         draw(userChoice, computerChoice);
         break;
     }
+    roundCounter++;
+    roundCount_div.innerHTML = `Round ${roundCounter}`
+    if (roundCounter == 5) {
+        restartGame();
+    }
+    }
+
+function restartGame() {
+    if (userScore > computerScore) {
+        roundAnn_p.innerHTML = "You won this game!👏👏👏"
+    } else if (computerScore > userScore) {
+        roundAnn_p.innerHTML = "You Loss this game... 💧"
+    } else {
+    roundAnn_p.innerHTML = "Draw...Try Again?";
+    }
+    // show a restart button
+    const tryAgain_btn = document.createElement('button');
+    tryAgain_btn.classList.add('tryAgain_btn');
+    tryAgain_btn.textContent = "Try again!";
+    choices_div.appendChild(tryAgain_btn);
+    //reload the page after clicking button 
+    tryAgain_btn.addEventListener('click', function(){
+        window.location.reload();
+    })
+    // no button after five rounds
+    rock_div.remove();
+    paper_div.remove();
+    scissors_div.remove();
 }
+ 
+
 
 function main() {
     rock_div.addEventListener('click', function() {
@@ -83,7 +116,7 @@ function main() {
     scissors_div.addEventListener('click', function() {
         game('s');
     })
+    
 }
 
 main();
-
